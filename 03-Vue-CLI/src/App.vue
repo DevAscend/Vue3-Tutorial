73b6cd294338e2d3bdbf9c89757ebce21f191a8d
DevAscend <71956581+DevAscend@users.noreply.github.com>
2021-01-08 15:47:08 -0500

    Add files via upload

---
+++ 03-Vue-CLI/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>Welcome to this tutorial</h1>
+  <h2>{{ fruit }}</h2>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      fruit: "Apple",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
