4b7f59f6b29157c80afa75bf53f19289f60d839d
DevAscend <71956581+DevAscend@users.noreply.github.com>
2021-01-12 15:42:02 -0500

    Add files via upload

---
+++ 05-Multiple-Components/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>Welcome to this tutorial</h1>
+  <h2 ref="subtitle">{{ fruit }}</h2>
+  <button @click="handleClick">Click me</button>
+  <Card />
+  <Card />
+  <Card />
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+export default {
+  name: "App",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      fruit: "Apple",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.subtitle);
+      this.$refs.subtitle.classList.add("custom-class");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
